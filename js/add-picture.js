5a489750c7bf754bedb7302721ab957d8da6c1c6
Ivan Lysukhin <ilysukhin@mail.ru>
2021-03-10 20:30:16 +0300

    Добавил возоможность открыть окно с фото и комментариями

--- js/add-picture.js
+++ js/add-picture.js
@@ -1,5 +1,8 @@
+import {openPopup, closePopup} from './utils.js';
+
 let templatePicture = document.querySelector('#picture').content.querySelector('a');
 let picturesContainer = document.querySelector('.pictures');
+let picturePopup = document.querySelector('.big-picture');
 
 let addPictures = (data) => {
   data.forEach((pic) => {
@@ -14,6 +17,66 @@ let addPictures = (data) => {
     commentsNumber.textContent = pic.comments.length;
 
     picturesContainer.appendChild(newPicture);
+
+    let closeButton = picturePopup.querySelector('.cancel');
+
+    newPicture.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      openPopup(picturePopup);
+
+      let bigPic = picturePopup.querySelector('.big-picture__img').querySelector('img');
+      bigPic.src = pic.url;
+
+      let bigLikes = picturePopup.querySelector('.likes-count');
+      bigLikes.textContent = pic.likes;
+
+      let bigComments = picturePopup.querySelector('.comments-count');
+      bigComments.textContent = pic.comments.length;
+
+      // Комментарии
+      let commentsList = picturePopup.querySelector('.social__comments');
+
+      pic.comments.forEach((comment) => {
+        let newComment = document.createElement('li');
+        newComment.classList.add('social__comment')
+
+        let newCommentPic = document.createElement('img');
+        newCommentPic.classList.add('social__picture');
+        newCommentPic.style.width = '35px';
+        newCommentPic.style.height = '35px';
+        newComment.appendChild(newCommentPic);
+
+        let newCommentText = document.createElement('p');
+        newCommentText.classList.add('social__text');
+        newComment.appendChild(newCommentText);
+
+        newCommentPic.src = comment.avatar;
+        newCommentPic.alt = comment.name;
+
+        newCommentText.textContent = comment.message;
+
+        commentsList.appendChild(newComment);
+      })
+
+      let bigDescription = picturePopup.querySelector('.social__caption');
+      bigDescription.textContent = pic.description;
+
+      closeButton.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        closePopup(picturePopup)
+        let comments = commentsList.querySelectorAll('li');
+        comments.forEach(comment => comment.remove());
+      });
+
+      document.addEventListener('keydown', (evt) => {
+        if (evt.keyCode === 27) {
+          closePopup(picturePopup);
+          let comments = commentsList.querySelectorAll('li');
+          comments.forEach(comment => comment.remove());
+        }
+      })
+    })
   })
 };
 
--- js/form.js
+++ js/form.js
@@ -1,31 +1,22 @@
+import {openPopup, closePopup} from './utils.js';
+
 let formPopup = document.querySelector('.img-upload__overlay');
-let pageBody = document.querySelector('body');
 let cancelButton = formPopup.querySelector('#upload-cancel');
 let uploadInput = document.querySelector('#upload-file');
 
-let openPopup = () => {
-  formPopup.classList.remove('hidden');
-  pageBody.classList.add('modal-open');
-}
-
-let closePopup = () => {
-  formPopup.classList.add('hidden');
-  pageBody.classList.remove('modal-open');
-}
-
 uploadInput.addEventListener('change', (evt)=> {
   evt.preventDefault();
-  openPopup();
+  openPopup(formPopup);
 });
 
 
 cancelButton.addEventListener('click', (evt) => {
   evt.preventDefault();
-  closePopup();
+  closePopup(formPopup);
 });
 
 document.addEventListener('keydown', (evt) => {
   if (evt.keyCode === 27) {
-    closePopup();
+    closePopup(formPopup);
   }
 });
